827d779ddac8cce53eef2a58e511b7f53179ea87
mbelnf1 <53024942+mbelnf1@users.noreply.github.com>
2019-07-26 23:01:08 -0400

    Add files via upload

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("Hello World");
+            Console.WriteLine(calc.Add(4,2));
+            Console.Read();
+        }
+    }
+}
---
+++ MyClassLibrary/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyClassLibrary
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int subtract(int x, int y)
+        {
+            return y - x;
+        }
+    }
+}
